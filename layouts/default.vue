769e1ee4d7ac6035ef3ae8703228bda1a33dd462
Lucas Daniel <lucasfelepe@gmail.com>
2021-06-05 19:46:37 -0300

    Criado página padrão de erros.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,43 @@
 <template>
   <div>
+    <b-navbar>
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <p>Find my <i>tasks!</i></p>
+        </b-navbar-item>
+      </template>
+      <template #start>
+        <b-navbar-item tag="nuxt-link" to="/">Principal</b-navbar-item>
+        <b-navbar-item tag="nuxt-link" to="cadastro/login"
+          >Informações</b-navbar-item
+        >
+        <b-navbar-item tag="nuxt-link" to="/login">Sobre nós</b-navbar-item>
+      </template>
+      <template #end>
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <button class="button is-info is-outlined is-rounded">
+              <strong>Cadastre-se</strong>
+            </button>
+            <button class="button is-info is-outlined is-rounded">
+              Entrar
+            </button>
+          </div>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
     <Nuxt />
   </div>
 </template>
+
 <script>
 export default {}
 </script>
-
-<style>
+<style scoped>
+p {
+  color: #023e8a;
+}
+.is-link {
+  color: #023e8a;
+}
 </style>
---
+++ layouts/error.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <section class="hero is-danger is-fullheight">
+      <div class="hero-body">
+        <div class="">
+          <p class="title">
+            Ops!<br />
+            Ocorreu um erro.
+          </p>
+          <div class="subtitle">
+            <p>Procure nosso suporte para mais detalhes.</p>
+            <br />
+            <b-button tag="nuxt-link" to="/"
+              >Voltar para página de início.</b-button
+            >
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'errorLayout',
+}
+</script>
+<style scoped>
+.hero-body {
+  justify-content: center;
+}
+p {
+  justify-content: center;
+  text-align: center;
+}
+div .subtitle {
+  justify-content: center;
+  text-align: center;
+}
+</style>

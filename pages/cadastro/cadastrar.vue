2ec30136b58a02774a1d4be1aba4dce465d8656b
Lucas Daniel <lucasfelepe@gmail.com>
2021-06-20 12:45:38 -0300

    Inclusão de icones fonts-awesome nos arquivos.

--- pages/cadastro/cadastrar.vue
+++ pages/cadastro/cadastrar.vue
@@ -4,39 +4,56 @@
       <div class="columns is-mobile is-centered">
         <div class="column is-4">
           <div class="box">
-            <b-image
-              :src="require('@/assets/images/logoPLan1.png')"
-              alt="PLan logo"
-              ratio="square"
-            ></b-image>
-            <b-field label="Nome" label-position="inside">
-              <b-input
-                placeholder="Ex: João de Barro"
-                maxlength="160"
-              ></b-input>
-            </b-field>
-            <b-field label="Apelido" label-position="inside">
-              <b-input placeholder="Joaozinho" maxlength="120"></b-input>
-            </b-field>
-            <b-field label="E-mail" label-position="inside">
-              <b-input
-                placeholder="joao.barro@gmail.com"
-                type="email"
-                maxlength="60"
-              ></b-input>
-            </b-field>
-            <b-field label="Senha" label-position="inside">
-              <b-input type="password" maxlength="30"></b-input>
-            </b-field>
-            <div class="block has-text-centered">
-              <b-button type="is-primary is-outlined" expanded
-                ><strong>Cadastrar!</strong></b-button
+            <div class="columns is-mobile is-centered">
+              <div class="column is-7">
+                <b-image
+                  :src="require('@/assets/images/logoPLan1.png')"
+                  alt="PLan logo"
+                ></b-image>
+              </div>
+            </div>
+            <form @submit.prevent="cadastrarUsuario">
+              <b-field label="Nome" label-position="inside" class="my-1">
+                <b-input
+                  placeholder="Ex: João de Barro"
+                  maxlength="160"
+                ></b-input>
+              </b-field>
+              <b-field label="Apelido" label-position="inside" class="my-1">
+                <b-input placeholder="Joaozinho" maxlength="120"></b-input>
+              </b-field>
+              <b-field label="E-mail" label-position="inside" class="my-1">
+                <b-input
+                  placeholder="joao.barro@gmail.com"
+                  type="email"
+                  maxlength="60"
+                  icon="envelope-square"
+                ></b-input>
+              </b-field>
+              <b-field label="Senha" label-position="inside" class="my-1">
+                <b-input type="password" maxlength="30"></b-input>
+              </b-field>
+              <div class="block has-text-centered my-1">
+                <b-button
+                  native-type="submit"
+                  type="is-primary is-outlined"
+                  expanded
+                  icon-right="user-plus"
+                >
+                  <strong>Cadastrar</strong></b-button
+                >
+              </div>
+            </form>
+            <div class="block has-text-centered mt-0">
+              <small
+                >Ao clicar em cadastrar, você concorda com os termos de
+                uso.</small
               >
             </div>
             <div class="block has-text-centered">
               <p>
-                Já possui uma conta? Clique
-                <NuxtLink to="/cadastro/entrar">aqui</NuxtLink>.
+                Já possui uma conta?
+                <NuxtLink to="/cadastro/entrar">Clique aqui</NuxtLink>.
               </p>
             </div>
           </div>
@@ -49,10 +66,21 @@
 <script>
 export default {
   layout: 'cadastro/cadastroPrincipal',
+  data() {
+    return {}
+  },
+  methods: {
+    cadastrarUsuario: (event) => {
+      alert('alo goiania!')
+    },
+  },
 }
 </script>
 <style>
 body {
   background-color: #15364f;
+}
+small {
+  font-size: 12px;
 }
 </style>>
--- pages/cadastro/entrar.vue
+++ pages/cadastro/entrar.vue
@@ -4,30 +4,34 @@
       <div class="columns is-mobile is-centered">
         <div class="column is-4">
           <div class="box">
-            <b-image
-              :src="require('@/assets/images/logoPLan1.png')"
-              alt="PLan logo"
-              ratio="square"
-            ></b-image>
+            <div class="columns is-mobile is-centered">
+              <div class="column is-7">
+                <b-image
+                  :src="require('@/assets/images/logoPLan1.png')"
+                  alt="PLan logo"
+                ></b-image>
+              </div>
+            </div>
             <b-field label="E-mail" label-position="inside">
               <b-input
                 placeholder="joao.barro@gmail.com"
                 type="email"
                 maxlength="60"
+                icon="at"
               ></b-input>
             </b-field>
             <b-field label="Senha" label-position="inside">
               <b-input type="password" maxlength="30"></b-input>
             </b-field>
             <div class="block has-text-centered">
-              <b-button type="is-primary is-outlined" expanded
-                ><strong>Login!</strong></b-button
+              <b-button type="is-primary is-outlined" expanded icon-right="user"
+                ><strong>Entrar</strong></b-button
               >
             </div>
             <div class="block has-text-centered">
               <p>
-                Não possui uma conta? Clique
-                <NuxtLink to="/cadastro/cadastrar">aqui</NuxtLink> e
+                Não possui uma conta?
+                <NuxtLink to="/cadastro/cadastrar">Clique aqui</NuxtLink> e
                 cadastre-se!
               </p>
             </div>
@@ -44,6 +48,9 @@
 <script>
 export default {
   layout: 'cadastro/cadastroPrincipal',
+  data() {
+    return {}
+  },
 }
 </script>
 <style>

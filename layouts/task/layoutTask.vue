4b2a6b49ae84434cf45d67d29eb316116817a8c5
LukeDaniel16 <lucasfelepe@gmail.com>
2021-08-21 21:28:11 -0300

    Alterações

--- layouts/task/layoutTask.vue
+++ layouts/task/layoutTask.vue
@@ -2,7 +2,7 @@
   <div>
     <b-tabs>
       <b-tab-item label="Pictures" icon="google-photos"></b-tab-item>
-      <b-tab-item label="Music" icon="library-music"></b-tab-item>
+      <b-tab-item label="Musicssss" icon="library-music"></b-tab-item>
       <b-tab-item label="Videos" icon="video"></b-tab-item>
     </b-tabs>
     <Nuxt />
